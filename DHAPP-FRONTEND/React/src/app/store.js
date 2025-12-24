9f4474d931e7d8848983d6913426f838bb289126
thejas199966-a11y <thejas199966@gmail.com>
2025-12-24 20:17:26 +0530

    Delete DHAPP-FRONTEND/React directory

--- DHAPP-FRONTEND/React/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import driverReducer from "../features/driverSlice";
-
-export const store = configureStore({
-  reducer: {
-    drivers: driverReducer,
-  },
-});
